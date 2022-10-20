81337f1dbc73ea9aa342591a3c85950c0beeb8b2
Leandro-Frota <108706993+Leandro-Frota@users.noreply.github.com>
2022-10-19 22:53:18 -0300

    Remeando arquivos Cap01 e add arquivos Cap02

---
+++ Cap02/js/ex2_2.js
@@ -0,0 +1,19 @@
+// cria referência ao form e oas elementos h3 e h4 (resposta)
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
+
+// cria "ouvinte" de evento, acionado quando o botão submit for clicado
+
+frm.addEventListener("submit",(e)=> { 
+    const titulo = frm.inTitulo.value   // obtem o conteudo dos campos
+    const duracao = Number(frm.inDuracao.value)
+
+    const horas = Math.floor(duracao /60) // arredonda para baixo resultado
+    const minutos = duracao % 60   // obtem o resto da divisão
+
+    resp1.innerText = titulo  // exibe as respostas
+    resp2.innerText = `${horas} hora(s) e ${minutos} minuto(s)`
+
+    e.preventDefault()
+})
